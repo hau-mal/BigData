849a103b4c2d5de52ca32c6fd32de4f342d3f70f
Hauke Mallow <hau-mal@users.noreply.github.com>
2017-05-01 20:37:36 +0200

    Update nifi-install.sh

--- sh/nifi-install.sh
+++ sh/nifi-install.sh
@@ -3,32 +3,13 @@
 # Install Build Tools
 cd $HOME
 
-sudo /bin/date +%H:%M:%S > install.progress.txt
-
-echo "ooooo      FULL INSTALL      ooooo" >> install.progress.txt
-
 # Install Java
-echo "Installing openjdk-8-jdk package" >> install.progress.txt
-
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get -y update
 sudo apt-get install -y openjdk-8-jdk
 sudo apt-get -y update --fix-missing
 sudo apt-get install -y openjdk-8-jdk
-
 sudo ln -s /usr/lib/jvm/java-8-openjdk-amd64/ /usr/lib/jvm/default-java
-
-sudo /bin/date +%H:%M:%S >> install.progress.txt
-
-
-# Install Java build tools
-echo "Installing ant package" >> install.progress.txt
-sudo apt-get -y install ant
-sudo /bin/date +%H:%M:%S >> install.progress.txt
-
-echo "Installing maven package" >> install.progress.txt
-sudo apt-get -y install maven
-sudo /bin/date +%H:%M:%S >> install.progress.txt
 
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> .bashrc
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
@@ -43,7 +24,5 @@ sudo /usr/bin/nifi/nifi-1.1.2/bin/nifi.sh install dataflow
 sudo /usr/bin/nifi/nifi-1.1.2/bin/nifi.sh start 
 
 cd $HOME
-
-sudo /bin/date +%H:%M:%S >> install.progress.txt
 
 echo "nifi installation done"
