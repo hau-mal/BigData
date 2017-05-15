ad5d8516a563d6fd72174a59948f3c0b1308c999
Hauke Mallow <hau-mal@users.noreply.github.com>
2017-05-15 18:15:31 +0200

    Update nifi-install.sh

--- nifi/vm-template/scripts/nifi-install.sh
+++ nifi/vm-template/scripts/nifi-install.sh
@@ -25,6 +25,9 @@ sudo wget ftp://apache.mirrors.tds.net/pub/apache.org/nifi/1.1.2/nifi-1.1.2-bin.
 sudo tar -xzf nifi-1.1.2-bin.tar.gz
 sudo /usr/bin/nifi/nifi-1.1.2/bin/nifi.sh install dataflow
 sudo /usr/bin/nifi/nifi-1.1.2/bin/nifi.sh start 
+# Increase the number of TCP socket ports available
+sudo sysctl -w net.ipv4.ip_local_port_range="10000 65000"
+
  
 sudo /bin/date +%H:%M:%S >> /home/$1/install.progress.txt
 
